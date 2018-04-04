85994cb1e5f79ece9bfc5678cb9f44ec6b5f8b40
Mariam Issa <miss.ethawy@gmail.com>
2018-04-04 09:40:07 -0500

    Initial setup.

---
+++ PhotoLibrary/PhotoLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoLibrary
+//
+//  Created by Mariam Issa on 4/4/18.
+//  Copyright © 2018 Mariam Al Ethawy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoLibrary/PhotoLibrary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoLibrary
+//
+//  Created by Mariam Issa on 4/4/18.
+//  Copyright © 2018 Mariam Al Ethawy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
